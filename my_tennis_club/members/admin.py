157c52cb3bc0866877a7eac498ff6378762fb795
Jeanbg33 <jeanbogadi33@gmail.com>
2025-09-13 13:59:20 -0400

    Feat: FIN

--- my_tennis_club/members/admin.py
+++ my_tennis_club/members/admin.py
@@ -6,9 +6,5 @@ from .models import Member
 class MemberAdmin(admin.ModelAdmin):
   list_display = ("firstname", "lastname", "joined_date",)
   prepopulated_fields = {"slug": ("firstname", "lastname")}
-  
+
 admin.site.register(Member, MemberAdmin)
-
-
-  
-  
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -10,8 +10,8 @@ def members(request):
   }
   return HttpResponse(template.render(context, request))
   
-def details(request, id):
-  mymember = Member.objects.get(id=id)
+def details(request, slug):
+  mymember = Member.objects.get(slug=slug)
   template = loader.get_template('details.html')
   context = {
     'mymember': mymember,
@@ -89,10 +89,10 @@ def testing(request):
 '''
 
 
+
 def testing(request):
-  mydata = Member.objects.filter(firstname__startswith='m').values()
   template = loader.get_template('template.html')
   context = {
-    'mymembers': mydata,
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
   }
   return HttpResponse(template.render(context, request))
